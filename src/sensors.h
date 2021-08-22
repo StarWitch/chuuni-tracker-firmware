6309e9d95c01d6a04d7419b4b689a74c2fe29874
Alice D <alice@starwitch.productions>
2021-08-22 13:40:21 -0400

    cleanup

--- src/sensors.h
+++ src/sensors.h
@@ -1,9 +1,6 @@
-#ifndef wifi_imu_sensors_h
-#define wifi_imu_sensors_h
+#pragma once
 
 #include <SparkFun_BNO080_Arduino_Library.h>
 #include <SparkFun_I2C_Mux_Arduino_Library.h>
 
 BNO080 **IMUSensor;
-
-#endif // wifi_imu_sensors_h
